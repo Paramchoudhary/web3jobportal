afc0a5cb8c47e6ed122a9efac2b0608ae6f44dd0
Param Choudhary <69947192+Paramchoudhary@users.noreply.github.com>
2024-09-24 13:10:10 +0530

    Update Blog1.jsx

--- src/components/pages/Blog1.jsx
+++ src/components/pages/Blog1.jsx
@@ -1,1 +1,41 @@
 
+import React from "react";
+
+
+const Blog = () => {
+  return (
+    <div className="blog-container">
+      <div className="blog-header">
+        <h1 className="blog-title">Web3 Developer Roadmap for One Year</h1>
+
+        <p className="blog-paragraph">
+    
+In the fast-growing Web3 space, having a clear plan is key to becoming a skilled developer. Here's a streamlined roadmap to guide you over one year:
+<br />
+<br />
+<h3 className="blog-subheading">First 6 Months - Learn the Fundamentals:</h3><br />
+1. <b> Blockchain Basics:</b>  Understand how blockchain works. <br />
+2.  <b> Bitcoin & Ethereum</b>: Study their mechanics and ecosystems. <br />
+3.  <b> JavaScript/TypeScript</b>: Build a strong programming foundation. <br />
+4.  <b> ReactJS/NextJS</b>: Master frontend development. <br />
+5.  <b> Solidity</b>: Dive into smart contract coding. <br />
+6.  <b> Foundry</b>: Explore testing and deployment frameworks. <br />
+7.  <b> SDK/Libraries</b>: Learn how to interact with blockchains. <br />
+<br />
+<h3 className="blog-subheading"> Last 6 Months - Build Projects:</h3> <br />
+1. <b> ERC-20 Token</b>: Create your own cryptocurrency. <br />
+2.<b> NFT Mint Website</b>: Launch an NFT platform. <br />
+3. <b> NFT Marketplace</b>: Build a decentralized marketplace for NFTs. <br />
+4. <b> DEX</b>: Develop a decentralized exchange. <br />
+5.<b> D-Social</b>: Create a decentralized social media platform. <br />
+6.<b> Uniswap Clon</b>: Replicate a popular DeFi protocol. <br />
+7.<b> Account Abstraction Wallet</b>: Design a user-friendly wallet with custom functionality. <br />
+ </p>
+       
+     
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
